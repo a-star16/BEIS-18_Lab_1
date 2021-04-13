1d1341d76a0fd2d31a2af86e2d38d121118a3f1e
a-star16 <81609615+a-star16@users.noreply.github.com>
2021-04-13 09:14:22 +0300

    Add files via upload
    Добавление программы

---
+++ Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+static void Main (string[] args)  // - началоописанияфункции.
+                               //изменяем заголовок окна консоли:
+    {
+        Console.Title = "Пример использования инструментов класса Console";
+        //изменяемцветфона:  
+        Console.BackgroundColor = ConsoleColor.Black;
+        //изменяемцветтекста% 
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Clear();// - очисткаокна
+        Console.Write(" Привет, студент! \n\n ");// \n - перевод строки 
+    }                                // конец описания функции
+}
+}
+
